a6de950110842307bc17e4a05ac7ddd1b425cac6
Tim Chevalier <tjc@igalia.com>
2025-03-27 19:09:19 -0700

    Add tests for calling TCP bind on sockets in various states

--- crates/test-programs/src/bin/sockets_0_3_tcp_bind.rs
+++ crates/test-programs/src/bin/sockets_0_3_tcp_bind.rs
@@ -148,6 +148,157 @@ fn test_tcp_bind_dual_stack() {
     assert!(matches!(sock.bind(addr), Err(ErrorCode::InvalidArgument)));
 }
 
+/// State of socket must be TcpState::Default
+async fn test_tcp_bind_already_connected() {
+    let sock = TcpSocket::new(IpAddressFamily::Ipv4);
+    let ip = IpAddress::new_loopback(IpAddressFamily::Ipv4);
+
+    // (copied from test_tcp_connect_explicit_bind() in sockets_0_3_tcp_connect.rs)
+    let listener = {
+        let bind_address = IpSocketAddress::new(ip, 0);
+        let listener = TcpSocket::new(IpAddressFamily::Ipv4);
+        listener.bind(bind_address).unwrap();
+        listener.listen();
+        listener
+     };
+
+    let listener_address = listener.local_address().unwrap();
+    sock.bind(listener_address);
+
+    let connect_result = sock.connect(listener_address).await;
+    assert!(connect_result.is_ok());
+    // Calling bind() on the already-connected socket should fail
+    assert_eq!(sock.bind(listener_address), Err(ErrorCode::InvalidState));
+}
+
+/// State of socket must be TcpState::Default
+fn test_tcp_bind_already_bound() {
+    let sock = TcpSocket::new(IpAddressFamily::Ipv4);
+    let addr = IpSocketAddress::new(IpAddress::new_loopback(IpAddressFamily::Ipv4), 0);
+    assert!(sock.bind(addr).is_ok());
+    // Calling bind() on the already-bound socket should fail
+    assert_eq!(sock.bind(addr), Err(ErrorCode::InvalidState));
+}
+
+/// State of socket must be TcpState::Default
+fn test_tcp_bind_already_listening() {
+    let sock = TcpSocket::new(IpAddressFamily::Ipv4);
+    let addr = IpSocketAddress::new(IpAddress::new_loopback(IpAddressFamily::Ipv4), 0);
+    assert!(sock.bind(addr).is_ok());
+    assert!(sock.listen().is_ok());
+    // Calling bind() on the already-listening socket should fail
+    assert_eq!(sock.bind(addr), Err(ErrorCode::InvalidState));
+}
+
+// I don't think we can test for bind() when the socket state is Connecting,
+// because there's no way to say "make this call after connect() starts
+// but before it returns"
+/*
+/// State of socket must be TcpState::Default
+async fn test_tcp_bind_already_connecting() {
+    let sock = TcpSocket::new(IpAddressFamily::Ipv4);
+    let ip = IpAddress::new_loopback(IpAddressFamily::Ipv4);
+
+    // (copied from test_tcp_connect_explicit_bind() in sockets_0_3_tcp_connect.rs)
+    let listener = {
+        let bind_address = IpSocketAddress::new(ip, 0);
+        let listener = TcpSocket::new(IpAddressFamily::Ipv4);
+        listener.bind(bind_address).unwrap();
+        // Listener is not actually listening
+        listener
+     };
+
+    let listener_address = listener.local_address().unwrap();
+    let bind_result = sock.bind(IpSocketAddress::new(ip, 0));
+    assert!(bind_result.is_ok());
+
+    // Attempt to re-bind while socket is still in "connecting" state;
+    // this should fail.
+    let connect_future = sock.connect(listener_address).await;
+    let bind_result_2 = sock.bind(IpSocketAddress::new(ip, 0));
+    assert!(!bind_result_2.is_ok());
+}
+ */
+
+/// State of socket must be TcpState::Default
+async fn test_tcp_bind_already_closed() {
+    let sock = TcpSocket::new(IpAddressFamily::Ipv4);
+    let bind_address = IpSocketAddress::new(IpAddress::new_loopback(IpAddressFamily::Ipv4), 0);
+
+    let listener = {
+        let listener = TcpSocket::new(IpAddressFamily::Ipv4);
+        listener.bind(bind_address).unwrap();
+        // Listener is not actually listening
+        listener
+     };
+
+    let listener_address = listener.local_address().unwrap();
+
+    // The first bind should succeed
+    assert!(sock.bind(bind_address).is_ok());
+    // Try to connect; the server isn't listening
+    assert!(sock.connect(listener_address).await.is_err());
+    // The second bind() should fail because the socket is now closed
+    assert_eq!(sock.bind(bind_address), Err(ErrorCode::InvalidState));
+}
+
+
+/*
+The following causes a panic:
+
+thread '<unnamed>' panicked at /home/tjc/.cargo/git/checkouts/wit-bindgen-e92a7091d4f51398/3df706c/crates/guest-rust/rt/src/async_support/stream_support.rs:375:48:
+internal error: entered unreachable code
+
+The goal of this test was to put a socket into the TcpState::Error state. Still not sure how to create that condition.
+
+Similar to test_tcp_shutdown_should_not_lose_data() from sockets_0_3_tcp_streams.rs, but without the server.receive() call
+ */
+
+/// State of socket must be TcpState::Default
+async fn test_tcp_bind_already_error() {
+    let ip = IpAddress::new_loopback(IpAddressFamily::Ipv4);
+    let bind_address = IpSocketAddress::new(ip, 0);
+    let listener = TcpSocket::new(IpAddressFamily::Ipv4);
+    listener.bind(bind_address).unwrap();
+    let mut accept = listener.listen().unwrap();
+    let bound_address = listener.local_address().unwrap();
+    let client_socket = TcpSocket::new(IpAddressFamily::Ipv4);
+    let ((), accepted_socket) = join!(
+        async {
+            client_socket.connect(bound_address).await.unwrap();
+        },
+        async {
+            let mut accepted_socket = accept.next().await.unwrap().unwrap();
+            assert_eq!(accepted_socket.len(), 1);
+            accepted_socket.pop().unwrap();
+        },
+    );
+
+    // Create a significantly bigger buffer, so that we can be pretty sure the `write` won't finish immediately:
+    let big_buffer_size = 100 * 1024;
+    let outgoing_data = vec![0; big_buffer_size];
+
+    let (mut client_tx, client_rx) = wit_stream::new();
+
+    // Idea: drop should occur while send is in progress.
+    // (We hope)
+    join!(
+        async {
+            client_tx.send(outgoing_data.clone()).await.unwrap();
+            client_tx.close().await.unwrap();
+            drop(client_tx);
+        },
+        async {
+            client_socket.send(client_rx).await.unwrap();
+        },
+    );
+
+    // Listener should now be in the error state
+    // Socket is already in an error state; calling bind() on it should fail
+    assert_eq!(listener.bind(bind_address), Err(ErrorCode::InvalidState));
+}
+
+
 impl test_programs::p3::exports::wasi::cli::run::Guest for Component {
     async fn run() -> Result<(), ()> {
         const RESERVED_IPV4_ADDRESS: IpAddress = IpAddress::Ipv4((192, 0, 2, 0)); // Reserved for documentation and examples.
@@ -181,6 +332,20 @@ impl test_programs::p3::exports::wasi::cli::run::Guest for Component {
         test_tcp_bind_non_unicast();
 
         test_tcp_bind_dual_stack();
+
+        test_tcp_bind_already_connected().await;
+
+        test_tcp_bind_already_bound();
+
+        test_tcp_bind_already_listening();
+
+        // See comment
+        // test_tcp_bind_already_connecting().await;
+
+        // See comment
+        // test_tcp_bind_already_error().await;
+
+        test_tcp_bind_already_closed().await;
 
         Ok(())
     }
